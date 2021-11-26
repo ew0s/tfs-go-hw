c825debf77287ae304295c00bf913b58f498e47c
ew0s <ewos365@mail.ru>
2021-11-27 00:58:34 +0300

    Fix: schema updated

--- course_project/trade-bot/schema/000001_init.up.sql
+++ course_project/trade-bot/schema/000001_init.up.sql
@@ -7,3 +7,26 @@ CREATE TABLE users
     public_api_key  varchar(255) not null,
     private_api_key varchar(255) not null
 );
+
+CREATE TABLE orders
+(
+    id                    serial       not null unique,
+    order_id              varchar(255) not null unique,
+    user_id               integer      not null,
+    cli_order_id          varchar(255) not null,
+    type                  varchar(255) not null,
+    symbol                varchar(255) not null,
+    quantity              float8       not null,
+    side                  varchar(255) not null,
+    filled                float8       not null,
+    timestamp             varchar(255) not null,
+    last_update_timestamp varchar(255) not null,
+    limit_price           float8
+);
+
+CREATE TABLE users_orders
+(
+    id       serial                                                      not null unique,
+    user_id  int references users (id) on delete cascade                 not null,
+    order_id varchar(255) references orders (order_id) on delete cascade not null
+)
